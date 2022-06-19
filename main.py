03045767c2015e6d55fad03948cd03dedd2961eb
dbCoder <96175955+dbcoder14@users.noreply.github.com>
2022-06-19 20:30:14 +0200

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-basic.show_icon(IconNames.HEART)
-basic.pause(1000)
-basic.show_icon(IconNames.DUCK)
-basic.pause(1000)
-basic.show_icon(IconNames.HOUSE)
-basic.pause(1000)
+number = 0
+
+def on_gesture_shake():
+    global number
+    basic.clear_screen()
+    number = randint(0, 10)
+    if number < 3:
+        basic.show_icon(IconNames.YES)
+    elif number > 3:
+        basic.show_icon(IconNames.NO)
+    else:
+        basic.show_icon(IconNames.DIAMOND)
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
--- main.ts
+++ main.ts
@@ -1,11 +1,14 @@
-basic.forever(function () {
-    basic.showIcon(IconNames.Duck)
-    basic.showLeds(`
-        . . . . .
-        . # # . .
-        # # # . .
-        . # # # #
-        . # # # .
-        `)
-    basic.pause(500)
+let number = 0
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    
+    basic.clearScreen()
+    number = randint(0, 10)
+    if (number < 3) {
+        basic.showIcon(IconNames.Yes)
+    } else if (number > 3) {
+        basic.showIcon(IconNames.No)
+    } else {
+        basic.showIcon(IconNames.Diamond)
+    }
+    
 })
